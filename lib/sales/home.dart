22deb59b5df1cd9f60626b576133b56d8e7959cc
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-11-10 05:12:48 -0500

    added viewhome

---
+++ lib/sales/home.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:shop_team/sales/list_sale.dart';
+import 'package:shop_team/sales/products_view.dart';
+import 'sales_view.dart';
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Shop Team',style: TextStyle(
+
+        ),),
+      ),
+        body: SafeArea(child:GridView.count(
+        primary: false,
+        padding: const EdgeInsets.all(20),
+    crossAxisSpacing: 10,
+    mainAxisSpacing: 10,
+    crossAxisCount: 2,
+    children: <Widget>[
+      ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => ProductView(list: const <Item>[],),
+          ));
+        },
+        child: Text("Nueva venta"),
+      ),
+        ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) => SalesView(),
+            ));
+          },
+          child: Text("Ventas"),),
+    ],
+    ),),
+    );
+  }
+}
