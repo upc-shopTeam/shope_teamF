43415f5fa89b832b7544c386cd7a23978f62647a
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-11-10 05:14:11 -0500

    adde dsales view

---
+++ lib/sales/sales_view.dart
@@ -0,0 +1,169 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shop_team/api/sale.dart';
+import 'dart:convert';
+
+
+class SalesView extends StatefulWidget {
+  const SalesView({Key? key}) : super(key: key);
+
+  @override
+  State<SalesView> createState() => _SalesViewState();
+}
+
+class _SalesViewState extends State<SalesView> {
+
+  bool _expanded = false;
+
+  final url = Uri.parse("http://10.0.2.2:9000/api/invoices/");
+
+  late Future<List<Sale>> sales;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    sales = getProducts();
+
+
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: const Text('VENTAS')
+      ),
+      body: FutureBuilder<List<Sale>>(
+        future: sales,
+        builder: (context, snap) {
+          if(snap.hasData){
+            return ListView.builder(
+              itemCount: snap.data!.length,
+                itemBuilder: (context, index){
+
+                var sale = snap.data![index];
+                print(sale.sales);
+              //  var obj = List.from(jsonDecode(sale.sales[0]));
+
+                //int temp = sale.sales.length;
+                //temp--;
+                //var product = sale.sales![temp];
+                //print(product);
+                return Column(
+                    children: [
+                      Center(
+                        child: Container(
+                          margin: EdgeInsets.all(10),
+                          color: Colors.green,
+                          child: ExpansionPanelList(
+                            animationDuration: Duration(milliseconds: 2000),
+                            children: [
+                              ExpansionPanel(
+                                headerBuilder: (context, isExpanded) {
+                                  return ListTile(
+                                    title: Text("COD:  ${sale.id.substring(0,6)}", style: const TextStyle(color: Colors.black),),
+                                    trailing: Text('S/${sale.totalPayment.toString()}.00', style: const TextStyle(color: Colors.black),),
+                                  );
+                                },
+                                body:Container(
+                                  child: Column(
+                                    children:[
+                                      Padding(
+                                        padding: const EdgeInsets.only(bottom: 18),
+                                        child: Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                          children: [
+                                            Text('Cliente: ${sale.nameCustomer}',style: const TextStyle(color: Colors.black)),
+                                            Text('DNI: ${sale.dniCustomer}',style: const TextStyle(color: Colors.black)),
+                                          ],
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.only(bottom: 14),
+                                        child: Text(sale.date.replaceAll("T"," ",)),
+                                      ),
+                                      const Text("Productos:",
+                                          style: TextStyle(
+                                          fontWeight: FontWeight.bold
+                                      ),),
+                                      DataTable(
+                                          columns: const [
+                                            DataColumn(
+                                                label: Expanded(
+                                                  child: Text(
+                                                    'Producto',
+                                                    style: TextStyle(fontStyle: FontStyle.italic),
+                                                  ),
+                                                )),
+                                            DataColumn(
+                                                label: Expanded(
+                                                  child: Text(
+                                                    'Cantidad',
+                                                    style: TextStyle(fontStyle: FontStyle.italic),
+                                                  ),
+                                                )),
+                                            DataColumn(
+                                                label: Expanded(
+                                                  child: Text(
+                                                    'Precio',
+                                                    style: TextStyle(fontStyle: FontStyle.italic),
+                                                  ),
+                                                ))
+                                          ],
+                                          rows: List<DataRow>.generate(
+
+                                              sale.sales.length,
+                                                  (int index) => DataRow(cells: <DataCell>[
+                                                    //data[0]["name"]
+                                                DataCell(Text(sale.sales[index]["nameProduct"].toString())),
+                                                DataCell(Text(sale.sales[index]["amount"].toString())),
+                                                DataCell(Text(sale.sales[index]["unitPrice"].toString())),
+
+                                              ]))),
+
+                                    ],
+
+                                  ),
+                                ),
+                                isExpanded: _expanded,
+                                canTapOnHeader: true,
+                              ),
+                            ],
+                            dividerColor: Colors.grey,
+                            expansionCallback: (panelIndex, isExpanded) {
+                              _expanded = !_expanded;
+                              setState(() {});
+                            },
+                          ),
+                        ),
+                      ),
+                    ]
+                );
+                }
+
+            );
+          }
+          else if(snap.hasError) {
+            return  Center(
+              child: Text("error ${snap.error}"),
+            );
+
+          }
+          return const CircularProgressIndicator();
+        }
+      )
+
+          );
+
+  }
+  Future<List<Sale>> getProducts() async{
+    final res = await http.get(url); //text
+    final list = List.from(jsonDecode(res.body));
+    List<Sale> sales = [];
+    list.forEach((element) {
+      final Sale product = Sale.fromJson(element);
+      sales.add(product);
+    });
+    return sales;
+  }
+
+}
