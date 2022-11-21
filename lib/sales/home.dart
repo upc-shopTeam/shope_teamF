e685cee18c9b91efaaeae824cd4ec4621ea3a05f
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-11-20 21:34:30 -0500

    feat: added button for logout

--- lib/sales/home.dart
+++ lib/sales/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shop_team/sales/list_sale.dart';
 import 'package:shop_team/sales/products_view.dart';
+import '../sign-in/sign-in_view.dart';
 import 'sales_view.dart';
 class Home extends StatelessWidget {
 //String dataId;
@@ -10,31 +12,46 @@ Home();
   Widget build(BuildContext context) {
     return  Scaffold(
 
-        body: SafeArea(
-          child:GridView.count(
-        primary: false,
-        padding: const EdgeInsets.all(20),
+        body: Column(
+          children: [
+            SafeArea(
+              child:GridView.count(
+                shrinkWrap: true,
+
+                primary: false,
+            padding: const EdgeInsets.all(20),
     crossAxisSpacing: 10,
     mainAxisSpacing: 10,
     crossAxisCount: 2,
     children: <Widget>[
       ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-            builder: (context) => ProductView(list: const <Item>[],),
-          ));
-        },
-        child: Text("Nueva venta"),
+            onPressed: () {
+              Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => ProductView(list: const <Item>[],),
+              ));
+            },
+            child: Text("Nueva venta"),
       ),
-        ElevatedButton(
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(
-              builder: (context) => SalesView(),
-            ));
-          },
-          child: Text("Ventas"),),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(MaterialPageRoute(
+                  builder: (context) => SalesView(),
+                ));
+              },
+              child: Text("Ventas"),),
     ],
     ),),
+            OutlinedButton.icon(
+              onPressed: () async {
+                SharedPreferences preferences = await SharedPreferences.getInstance();
+                await preferences.clear();
+                Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>SignInView()), (route) => false);
+              },
+              icon:Icon(Icons.login),
+              label: Text("Logout"),
+            )
+          ],
+        ),
     );
   }
 }
