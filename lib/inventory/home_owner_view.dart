8e2350cc3ed97a43ec1cd6035ffd602554e77f48
Nilton Torres <89040283+LITO-TR@users.noreply.github.com>
2022-11-24 03:40:58 -0500

    refactor: styles fix

--- lib/inventory/home_owner_view.dart
+++ lib/inventory/home_owner_view.dart
@@ -4,14 +4,12 @@ import 'package:flutter/material.dart';
 import 'package:jwt_decode/jwt_decode.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shop_team/inventory/balance_view.dart';
-import 'package:shop_team/inventory/employee_register.dart';
+import 'package:shop_team/inventory/employee_view.dart';
 import 'package:shop_team/inventory/invetory_view.dart';
 import 'package:shop_team/inventory/profile_owner.dart';
 import 'package:shop_team/inventory/sales_products_view.dart';
 import 'package:shop_team/sign-in/sign-in_view.dart';
 import 'package:http/http.dart' as http;
-
-import 'employee_view.dart';
 class HomeOwnerView extends StatefulWidget {
   const HomeOwnerView({Key? key}) : super(key: key);
 
@@ -24,8 +22,8 @@ class _HomeOwnerViewState extends State<HomeOwnerView> {
   String token = '';
   DateTime initDate= DateTime.now();
   String name = '';
-String nameShop = '';
-
+  String nameShop = '';
+String ownerId = '';
 
 
   @override
@@ -38,10 +36,11 @@ String nameShop = '';
     SharedPreferences preferences = await SharedPreferences.getInstance();
     String? val = preferences.getString("token");
     var objOwner = {};
+    var id = '';
     if(val!=null) {
       Map<String, dynamic> payload = Jwt.parseJwt(val);
       final user = await http.get(Uri.parse("https://express-shopapi.herokuapp.com/api/owner/${payload["dataId"]}"));
-
+      id = payload["dataId"];
       objOwner = jsonDecode(user.body);
     }
 
@@ -51,11 +50,9 @@ String nameShop = '';
       initDate = DateTime.parse(objOwner["registerDate"]);
       name = objOwner["name"];
       nameShop = objOwner["nameShop"];
+      ownerId = id;
     });
-    print(initDate);
-    print(token);
-    print(name);
-    print(nameShop);
+
 
   }
 
@@ -63,10 +60,7 @@ String nameShop = '';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: new AppBar(
-        title: Text("DUEÑO"),
-        backgroundColor: Colors.green[600],
-      ),
+
       backgroundColor: Colors.green[100],
       body: SafeArea(
 
@@ -78,59 +72,25 @@ String nameShop = '';
 
 
               children: [
+                SizedBox(
+                  height: 30,
+                ),
                 OutlinedButton.icon(
                   style: OutlinedButton.styleFrom(
                     backgroundColor: Colors.white70,
 
                   ),
-
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => SalesProductsView(),
-                      ));
-                    },
-                    child: Text("Ventas por producto"),),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => BalanceView(initDate:initDate,),
-                      ));
-                    },
-                    child: Text("Balance"),),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => EmployeeRegister(),
-                      ));
-                    },
-                    child: Text("Agregar Trabajador"),),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => EmployeeView(),
-                      ));
-                    },
-                    child: Text("Trabajadores"),),
-                ],
-              ),
-              OutlinedButton.icon(
-                  onPressed: () async {
-                      SharedPreferences preferences = await SharedPreferences.getInstance();
-                      await preferences.clear();
-                      Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>SignInView()), (route) => false);
-
                   onPressed: ()  {
-                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ProfileOwner()));
+                    Navigator.of(context).push(MaterialPageRoute(builder: (context)=>ProfileOwner(id: ownerId )));
                   },
                   icon:Icon(Icons.person,
-                  color: Colors.black,
+                    color: Colors.black,
                   ),
                   label: Text("Hola, $name",
                     style: TextStyle(
-                        color: Colors.black,
-                   ),
-                ),
+                      color: Colors.black,
+                    ),
+                  ),
                 ),
                 OutlinedButton.icon(
                   style: OutlinedButton.styleFrom(
@@ -139,13 +99,12 @@ String nameShop = '';
                   ),
 
                   onPressed: ()  {
-
                   },
                   icon:Icon(Icons.shop,
                     color: Colors.black,
                   ),
                   label: Text("TIENDA: $nameShop",style: TextStyle(
-                    color: Colors.black
+                      color: Colors.black
                   ),),
                 ),
                 GridView.count(
@@ -173,20 +132,20 @@ String nameShop = '';
                           child: Column(
                             mainAxisSize: MainAxisSize.min,
                             children:
-                              <Widget>[
-                                Icon(Icons.inventory_rounded,
-                                    size:70.0,
-                                   color: Colors.black,
-                                ),
-                                Text("Inventario",
+                            <Widget>[
+                              Icon(Icons.inventory_rounded,
+                                size:70.0,
+                                color: Colors.black,
+                              ),
+                              Text("Inventario",
 
                                 style: new TextStyle(
                                     fontSize: 17.0,
-                                  color: Colors.black
+                                    color: Colors.black
 
                                 ),
-                                ),
-                              ],
+                              ),
+                            ],
                           ),
                         ),
                       ),
@@ -231,11 +190,11 @@ String nameShop = '';
                           backgroundColor: Colors.white,
 
                         ),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => BalanceView(initDate:initDate,),
-                        ));
-                      },
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => BalanceView(initDate:initDate,),
+                          ));
+                        },
                         child: Center(
                           child: Padding(
                             padding: const EdgeInsets.all(21.0),
@@ -263,13 +222,12 @@ String nameShop = '';
                     ElevatedButton(
                         style: OutlinedButton.styleFrom(
                           backgroundColor: Colors.white,
-
                         ),
-                      onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => EmployeeRegister(),
-                        ));
-                      },
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => EmployeeView(),
+                          ));
+                        },
                         child: Center(
                           child: Padding(
                             padding: const EdgeInsets.all(21.0),
@@ -283,8 +241,8 @@ String nameShop = '';
                                 ),
                                 Text("Agregar Trabajador",
                                   style: new TextStyle(
-                                      fontSize: 17.0,
-                                      color: Colors.black,
+                                    fontSize: 17.0,
+                                    color: Colors.black,
                                   ),
                                 ),
                               ],
@@ -295,21 +253,21 @@ String nameShop = '';
                     ),
                   ],
                 ),
+
                 OutlinedButton.icon(
                   style: OutlinedButton.styleFrom(
                     backgroundColor: Colors.white70,
-
                   ),
-                    onPressed: () async {
-                        SharedPreferences preferences = await SharedPreferences.getInstance();
-                        await preferences.clear();
-                        Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>SignInView()), (route) => false);
-                    },
-                    icon:Icon(Icons.login,
+                  onPressed: () async {
+                    SharedPreferences preferences = await SharedPreferences.getInstance();
+                    await preferences.clear();
+                    Navigator.of(context).pushAndRemoveUntil(MaterialPageRoute(builder: (context)=>SignInView()), (route) => false);
+                  },
+                  icon:Icon(Icons.login,
                     color: Colors.black,),
-                    label: Text("Logout",style: TextStyle(
+                  label: Text("Logout",style: TextStyle(
                       color: Colors.black
-                    ),),
+                  ),),
                 )
               ],
             ),
@@ -318,4 +276,3 @@ String nameShop = '';
     );
   }
 }
-
